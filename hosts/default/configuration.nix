f36c2a25bd3293171746946a176327d3883ab048
landris006 <laczko.andris007@gmail.com>
2024-03-17 22:45:34 +0100

    ntfs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -26,6 +26,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.supportedFilesystems = [ "ntfs" ];
+
   services.pipewire = {
     enable = true;
     alsa = {
