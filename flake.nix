bc575715fffe8b02949c3329f5b0536005d4a94e
landris006 <laczko.andris007@gmail.com>
2024-10-15 22:02:53 +0200

    gaming tweaks

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Nixos config flake";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-gaming.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -8,6 +17,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-gaming.url = "github:fufexan/nix-gaming";
 
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=0f594732b063a90d44df8c5d402d658f27471dfe"; /* 0.43.0 */
 
--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,15 +1,54 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
-  programs.steam.enable = true;
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        softrealtime = "auto";
+        renice = 15;
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+    args = [
+      "--rt"
+      "--expose-wayland"
+    ];
+  };
+
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
+    platformOptimizations.enable = true;
+  };
+
+  services.pipewire.lowLatency.enable = true;
+
+  imports = with inputs.nix-gaming.nixosModules; [
+    pipewireLowLatency
+    platformOptimizations
+  ];
 
   environment.systemPackages = with pkgs; [
+    lutris
+    (wine.override { wineBuild = "wine64"; })
     (appimageTools.wrapType2
       {
         name = "awakened-poe-trade";
         src = fetchurl {
-          url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.23.10005/Awakened-PoE-Trade-3.23.10005.AppImage";
-          hash = "sha256-5lnEWKbu2jpNuzvjmW4j4Z895GPRRNdV2IbYE37518Q=";
+          url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v3.25.102/Awakened-PoE-Trade-3.25.102.AppImage";
+          hash = "sha256-lcdKJ+B8NQmyMsv+76+eeESSrfR/7Mq6svO5VKaoNUY=";
         };
       }
     )
