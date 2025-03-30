5b1b579abfd79a9778d42d7e539f8af8a2920c73
landris006 <laczko.andris007@gmail.com>
2025-03-30 14:44:41 +0200

    improved video saving

--- modules/home-manager/hypr/hyprland.nix
+++ modules/home-manager/hypr/hyprland.nix
@@ -15,13 +15,36 @@
       mainMod = "ALT_L";
       startReplay = pkgs.writeShellScriptBin "start-replay" ''
         pidof -q gpu-screen-recorder && exit 0
-        video_path="$HOME/Videos"
+        video_path="$HOME/Videos/gpu-screen-recorder"
         mkdir -p "$video_path"
-        ${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w screen -f 60 -a "default_output|default_input" -c mkv -bm cbr -q 45000 -r 180 -o "$video_path" \
-          && sleep 0.5 && notify-send -u low -- "GPU Screen Recorder" "Started recording in replay mode"
+        ${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder \
+          -w screen \
+          -f 60 \
+          -a "default_output|default_input" \
+          -c mkv \
+          -bm cbr -q 45000 \
+          -r 180 \
+          -o "$video_path" \
+          -sc ${saveVideo}/bin/save-video
+      '';
+      saveVideo = pkgs.writeShellScriptBin "save-video" ''
+        notify-send -u low -- "GPU Screen Recorder" "Saving replay..."
+
+        video_path=$1
+        video_directory=$(dirname "$video_path")
+
+        window_name="$(hyprctl activewindow -j | jq -r '.initialTitle')"
+        if [[ "$window_name" == "null" ]]; then
+          window_name="Desktop"
+        fi
+
+        mkdir -p "$video_directory/$window_name"
+        mv "$1" "$video_directory/$window_name"
+
+        notify-send -u low -- "GPU Screen Recorder" "Replay saved to $video_directory/$window_name"
       '';
       saveReplay = pkgs.writeShellScriptBin "save-replay" ''
-        killall -SIGUSR1 gpu-screen-recorder && sleep 0.5 && notify-send -u low -- "GPU Screen Recorder" "Replay saved"
+        killall -SIGUSR1 gpu-screen-recorder
       '';
     in {
       source = [
