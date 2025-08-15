ba6b55ac956ed77ba6a7ee28874a8d1f9e40f785
landris006 <laczko.andris007@gmail.com>
2025-08-15 12:12:04 +0200

    spicetify

--- modules/nixos/spotify.nix
+++ modules/nixos/spotify.nix
@@ -1,7 +1,46 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    spotify
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.spicetify-nix.nixosModules.default
   ];
+
+  programs.spicetify = let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+    enabledCustomApps = with spicePkgs.apps; [
+      newReleases
+      reddit
+      lyricsPlus
+      marketplace
+    ];
+    # theme = {
+    #   name = "Tokyo Night";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "evening-hs";
+    #     repo = "Spotify-Tokyo-Night-Theme";
+    #     rev = "d88ca06eaeeb424d19e0d6f7f8e614e4bce962be";
+    #     hash = "sha256-cLj9v8qtHsdV9FfzV2Qf4pWO8AOBXu51U/lUMvdEXAk=";
+    #   };
+    #
+    #   injectCss = true;
+    #   injectThemeJs = true;
+    #   replaceColors = true;
+    #   homeConfig = true;
+    #   overwriteAssets = false;
+    #   additonalCss = "";
+    # };
+    # colorScheme = "Night";
+  };
+
+  # environment.systemPackages = with pkgs; [
+  #   spotify
+  # ];
 
   # Local discovery (https://nixos.wiki/wiki/Spotify)
   # To sync local tracks from your filesystem with mobile devices in the same network,
