57abc207a769e609d1a99fb50d9a910b43ca19fa
Laczkó András <laczko.andris007@gmail.com>
2025-08-20 09:25:34 +0200

    overskride

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -1,5 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    overskride
+  ];
 }
