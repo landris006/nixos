b2eddb08a9c321dd641036cae4fe96266c15ea70
landris006 <laczko.andris007@gmail.com>
2025-11-13 08:49:53 +0000

    remove steam package override

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -30,23 +30,6 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
-    package = pkgs.steam.override {
-      extraLibraries = pkgs: [pkgs.xorg.libxcb];
-      extraPkgs = pkgs:
-        with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-          gamemode
-        ];
-    };
     extraCompatPackages = [
       pkgs.proton-ge-bin
     ];
