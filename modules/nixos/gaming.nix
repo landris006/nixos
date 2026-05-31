1cc89b85fd68f6d47f3802a6a98d0999beb8c5d4
landris006 <laczko.andris007@gmail.com>
2026-05-31 23:08:57 +0100

    gamescope fix

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -20,11 +20,7 @@
 
   programs.gamescope = {
     enable = true;
-    capSysNice = true;
-    args = [
-      "--rt"
-      "--expose-wayland"
-    ];
+    capSysNice = false;
   };
 
   programs.steam = {
